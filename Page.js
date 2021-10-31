1974128b7a19c2ce3debe26964c19e6167679d85
MrPleus <93022794+MrPleus@users.noreply.github.com>
2021-10-31 03:44:16 -0400

    Create Page.js

---
+++ Page.js
@@ -0,0 +1,8 @@
+ 
+ ​const​ ​Burger​ ​=​document​.​querySelector​(​".burger"​) 
+ ​const​ ​navlinks​ ​=​document​.​querySelector​(​".navlinks"​) 
+  
+ ​Burger​.​addEventListener​(​"click"​,​ ​(​)​ ​=>​ ​{ 
+ ​     ​navlinks​.​classList​.​toggle​(​"change"​) 
+ ​     ​Burger​.​classList​.​toggle​(​"active"​) 
+ ​}​)
